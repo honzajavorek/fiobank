3a5fcf078563a6e0dbae83cb2f2a4c39179b4136
Honza Javorek <mail@honzajavorek.cz>
2024-12-28 19:22:42 +0100

    re-introduce test coverage

--- fiobank.py
+++ fiobank.py
@@ -45,7 +45,7 @@ class ThrottlingError(Exception):
     """Throttling error raised when the API is being used too fast."""
 
     def __str__(self) -> str:
-        return "Token can be used only once per 30s."
+        return "Token can be used only once per 30s"
 
 
 class FioBank(object):
