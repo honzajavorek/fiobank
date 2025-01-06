c4fdf982ff94c55cb5215b4fe367435c4f2abf7c
Michal Čihař <michal@cihar.com>
2025-01-06 17:37:19 +0100

    feat: ship py.typed to allow type checking
    The py.typed needs to be present in the module to make type checking
    work (see PEP 561). In order to make it possible, the module can no
    longer be a single file, but needs to be installed in a directory.

--- src/fiobank/fiobank.py
+++ src/fiobank/fiobank.py
@@ -5,7 +5,6 @@ import warnings
 from collections.abc import Generator
 from datetime import date, datetime
 from decimal import Decimal
-from typing import Any, Callable
 
 import requests
 from tenacity import (
@@ -15,39 +14,8 @@ from tenacity import (
     wait_random_exponential,
 )
 
-
-__all__ = ("FioBank", "ThrottlingError")
-
-
-def coerce_amount(value: int | float) -> Decimal:
-    if isinstance(value, int):
-        return Decimal(value)
-    if isinstance(value, float):
-        return Decimal(str(value))
-    raise ValueError(value)
-
-
-def coerce_date(value: datetime | date | str) -> date:
-    if isinstance(value, datetime):
-        return value.date()
-    if isinstance(value, date):
-        return value
-    return datetime.strptime(value[:10], "%Y-%m-%d").date()
-
-
-def sanitize_value(value: Any, convert: Callable | None = None) -> Any:
-    if isinstance(value, str):
-        value = value.strip() or None
-    if convert and value is not None:
-        return convert(value)
-    return value
-
-
-class ThrottlingError(Exception):
-    """Throttling error raised when the API is being used too fast."""
-
-    def __str__(self) -> str:
-        return "Token can be used only once per 30s"
+from .exceptions import ThrottlingError
+from .utils import coerce_date, sanitize_value
 
 
 class FioBank:
